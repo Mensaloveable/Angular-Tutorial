25a6bef26a80c965a1910e69d9a8d823cc031b9a
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-10-23 18:38:23 +0100

    First Class

---
+++ basics/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // template: `<p>Hello LoveLives</p>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'basics';
+// }

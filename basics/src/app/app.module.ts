3b00b718949029178a88a1342eb61f08b3e2b0c9
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-10-25 10:40:08 +0100

    create new component

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -1,27 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-
-// import { AppComponent } from './app.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }

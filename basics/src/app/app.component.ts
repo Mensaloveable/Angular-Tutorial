975ef4ed1ec6c5f09a06fe9a4f68c607985f185a
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-10-25 06:50:18 +0100

    Interpolation

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -4,9 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // template: `<p>Hello LoveLives</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  str = 'Mensa';
+
+  getName() {
+    return this.str.toUpperCase();
+  }
+}
 // import { Component } from '@angular/core';
 
 // @Component({
